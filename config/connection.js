bffd6303b6afa4996460b2102552fb231921768f
wizeapps <jrgsolis@gmail.com>
2020-07-01 00:34:18 -0400

    Resolve Connection Heroku

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ var mysql = require("mysql");
 
 if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection({
-	  host: "u0zbt18wwjva9e0v.cbetxkdyhwsb.us",
+	  host: "u0zbt18wwjva9e0v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
 	  user: "ttgzqds9gpzbky00",
 	  password: "xz0jfxq6ifmvxdm7",
 	  database: "k1b8cdujshre0lfc"
