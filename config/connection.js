8e1bfda0a92ec0d5a77c4f115fa60906ab476cc7
wizeapps <jrgsolis@gmail.com>
2020-07-01 00:30:49 -0400

    Resolve Connection Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,12 @@
 var mysql = require("mysql");
 
 if (process.env.JAWSDB_URL) {
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
+	connection = mysql.createConnection({
+	  host: "u0zbt18wwjva9e0v.cbetxkdyhwsb.us",
+	  user: "ttgzqds9gpzbky00",
+	  password: "xz0jfxq6ifmvxdm7",
+	  database: "k1b8cdujshre0lfc"
+	});
 } else {
 	connection = mysql.createConnection({
 	  host: "localhost",
